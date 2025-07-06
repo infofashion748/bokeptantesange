86b44cd80cef50aa0a18700678813c75eb02bf35
infofashion748 <infofashion748@gmail.com>
2025-07-07 05:13:52 +0700

    Delete src/utils/site.ts

--- src/utils/site.ts
+++
@@ -1,9 +0,0 @@
-export const url = 'https://bokeptantesange.pages.dev';
-export const nama = 'Bokep Tante Sange';
-export const desk = 'Video Bokep Indo Sayang Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
-export const terbit = '2025-04-22T00:01:03Z';
-
-export const google='fjUYbLGMJFcyl9swPaeXuSAbwM5PUi01hxKdCXzw55I';
-export const yandex='63eb8aa2de652789';
-
-export const index='056d016a-3d9d-4630-bbc4-2c7f6e0f67cc';
